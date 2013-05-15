5809d7f78cc8dd834ef12155a2b91cff6c3f68c4
Ørjan Blom <blom@blom.tv>
2013-05-15 18:39:11 +0200

    Run specs in a random order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require "rspec-puppet"
 fixture_path = File.expand_path("../fixtures", __FILE__)
 
 RSpec.configure do |config|
-  config.module_path  = File.join(fixture_path, "modules")
   config.manifest_dir = File.join(fixture_path, "manifests")
+  config.module_path  = File.join(fixture_path, "modules")
+  config.order        = :random
 end
