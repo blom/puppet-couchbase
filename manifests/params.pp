8efe5b65eb17ff78f8d17a910a480ee00a9536e2
Ørjan Blom <blom@blom.tv>
2013-09-14 09:48:59 +0200

    Change package name

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
 # couchbase::params
 class couchbase::params {
-  $client_package_name = 'libcouchbase2'
+  $client_package_name = 'libcouchbase2-core'
   $server_package_name = 'couchbase-server'
   $server_service_name = 'couchbase-server'
 
--- spec/classes/couchbase_spec.rb
+++ spec/classes/couchbase_spec.rb
@@ -4,12 +4,12 @@ describe "couchbase" do
   it { should include_class "couchbase::params" }
   it { should include_class "couchbase::repository" }
 
-  it { should contain_package("libcouchbase2").
+  it { should contain_package("libcouchbase2-core").
               with_ensure("present").
               with_require(/\AClass\[Couchbase::Repository\]/) }
 
   context "when package_ensure is 'absent'" do
     let(:params) { {:package_ensure => "absent"} }
-    it { should contain_package("libcouchbase2").with_ensure("absent") }
+    it { should contain_package("libcouchbase2-core").with_ensure("absent") }
   end
 end
